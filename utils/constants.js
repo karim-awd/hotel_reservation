e15413066da048e85e63cd66d434535dabd5b042
karim-mohamed <k.mohamed3@hotmail.com>
2020-04-27 10:51:02 +0200

    adding to STAFF roles

--- utils/constants.js
+++ utils/constants.js
@@ -8,6 +8,6 @@ exports.generalApis = ['/login', '/signup']
 
 exports.permissions = {
     USER: ['/modify-order-status','/available-rooms', '/all-rooms' , '/all-orders', '/reserve-table', '/avail-table', '/reserve-meeting', '/end-meeting', '/all-meeting-rooms', '/all-reservations', '/modify-reservation-status'],
-    STAFF: ['/modify-order-status','/reserve-room', '/check-out', '/all-rooms', '/all-orders','/all-meeting-rooms', '/all-reservations', '/modify-reservation-status'],
+    STAFF: ['/modify-order-status','/reserve-room', '/check-out', '/all-rooms', '/all-orders','/all-meeting-rooms', '/all-reservations', '/modify-reservation-status', '/reserve-meeting', '/end-meeting'],
     ADMIN: ['/reserve-room', '/check-out']
 }
